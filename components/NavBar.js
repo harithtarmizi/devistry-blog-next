c25243a39b9f9b862536601f43a9e91f62011b5f
Rith <harith.trmz@gmail.com>
2023-02-27 16:13:04 +0800

    styling with sass

--- components/NavBar.js
+++ components/NavBar.js
@@ -3,18 +3,20 @@ import Link from "next/link";
 
 function NavBar() {
   return (
-    <nav>
-      <Link href="/">
-        <a>Devistry</a>
-      </Link>
+    <nav className="navBar">
+      <div className="container">
+        <Link href="/">
+          <a className="title">Devistry</a>
+        </Link>
 
-      <ul>
-        <li>
-          <Link href="/posts">
-            <a>All posts</a>
-          </Link>
-        </li>
-      </ul>
+        <ul>
+          <li>
+            <Link href="/posts">
+              <a>All posts</a>
+            </Link>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,13 @@
 import NavBar from "../components/NavBar";
+import "../styles/index.scss";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <NavBar />
-      <Component {...pageProps} />
+      <div className="container">
+        <Component {...pageProps} />
+      </div>
     </>
   );
 }
