8103de3f7aee9c45e395654946d8a7c8efaffa0e
Rith <harith.trmz@gmail.com>
2023-02-27 17:04:48 +0800

    building the post preview links

--- components/PostPreview.js
+++ components/PostPreview.js
@@ -2,10 +2,10 @@ import React from "react";
 
 function PostPreview({ post }) {
   return (
-    <>
+    <div className="postPreview">
       <h3>{post.title}</h3>
       <p>{post.description}</p>
-    </>
+    </div>
   );
 }
 
