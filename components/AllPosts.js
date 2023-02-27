7def5e75558aebaf65972f227219aedf188ee8bc
Rith <harith.trmz@gmail.com>
2023-02-27 16:48:24 +0800

    bulding the posts overview

---
+++ components/AllPosts.js
@@ -0,0 +1,20 @@
+import React from "react";
+import PostPreview from "./PostPreview";
+
+function AllPosts({ posts }) {
+
+  function renderPostPreviews() {
+    return posts.map((post) => {
+      return <PostPreview post={post} key={post.id} />
+    })
+  }
+
+  return (
+    <>
+      <h2>Posts</h2>
+      {renderPostPreviews()}
+    </>
+  );
+}
+
+export default AllPosts;
--- pages/posts.js
+++ pages/posts.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+import axios from "axios";
+import AllPosts from "../components/AllPosts";
 
-function Posts() {
+function Posts({ posts }) {
   return (
-    <div>All posts</div>
-  )
+    <div>
+      <AllPosts posts={posts} />
+    </div>
+  );
 }
 
 export default Posts;
+
+export async function getStaticProps() {
+  const postRes = await axios.get("http://localhost:1337/posts");
+
+  return {
+    props: {
+      posts: postRes.data,
+    },
+  };
+}
